0df08cc3684f71471e47e44557915b9e8813b107
George Tirnovan <94456265+goliath3413@users.noreply.github.com>
2022-10-10 14:46:04 +0200

    self made. may change for requirements

--- spaceInvader.js
+++ spaceInvader.js
@@ -1,3 +1,84 @@
 "use strict";
 
-alert("Draw your space invader here");
+let canvas = document.querySelector("canvas");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let context = canvas.getContext('2d');
+
+joe();
+
+function joe() {
+    context.fillRect(0, 0, 300, 300);
+    context.stroke();
+    
+    context.beginPath();
+    context.rect(50, 0, 50, 50);
+    context.fillStyle = '#7FFF00';
+    context.fill();
+
+    context.beginPath();
+    context.rect(50, 50, 50, 50);
+    context.fillStyle = '#7FFF00';
+    context.fill();
+
+    context.beginPath();
+    context.rect(0, 50, 50, 50);
+    context.fillStyle = '#7FFF00';
+    context.fill();
+
+    context.beginPath();
+    context.rect(200, 0, 50, 50);
+    context.fillStyle = '#7FFF00';
+    context.fill();
+
+    context.beginPath();
+    context.rect(200, 50, 50, 50);
+    context.fillStyle = '#7FFF00';
+    context.fill();
+
+    context.beginPath();
+    context.rect(250, 50, 50, 50);
+    context.fillStyle = '#7FFF00';
+    context.fill();
+
+    context.beginPath();
+    context.rect(50, 200, 50, 50);
+    context.fillStyle = '#7FFF00';
+    context.fill();
+
+    context.beginPath();
+    context.rect(0, 200, 50, 50);
+    context.fillStyle = '#7FFF00';
+    context.fill();
+
+    context.beginPath();
+    context.rect(50, 150, 50, 50);
+    context.fillStyle = '#7FFF00';
+    context.fill();
+
+    context.beginPath();
+    context.rect(100, 200, 50, 50);
+    context.fillStyle = '#7FFF00';
+    context.fill();
+
+    context.beginPath();
+    context.rect(150, 200, 50, 50);
+    context.fillStyle = '#7FFF00';
+    context.fill();
+
+    context.beginPath();
+    context.rect(200, 150, 50, 50);
+    context.fillStyle = '#7FFF00';
+    context.fill();
+
+    context.beginPath();
+    context.rect(250, 200, 50, 50);
+    context.fillStyle = '#7FFF00';
+    context.fill();
+
+    context.beginPath();
+    context.rect(200, 200, 50, 50);
+    context.fillStyle = '#7FFF00';
+    context.fill();
+
+}
